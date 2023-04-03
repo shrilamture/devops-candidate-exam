2fbbdfeb0f2d8a9dcee2647f5c6b29886dc1821e
shrilamture <91794701+shrilamture@users.noreply.github.com>
2023-04-03 12:40:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
 provider "aws" {
   region  = "ap-south-1" # Don't change the region
 }
-
-# Add your S3 backend configuration here
+# Backend 
+terraform {
+ backendbackend "s3" {
+ encrypt = true
+ bucket = "3.devops.candidate.exam"
+ region = ap-south-1
+ key = shrikant.lamture
+ }
+}
